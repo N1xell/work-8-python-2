161f5c8b65819a9abf836cb4a7161b0a8afc5220
N1xell <162792845+N1xell@users.noreply.github.com>
2024-10-18 22:03:52 +0300

    Update server.js

---
+++ config/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const menuRoutes = require('../routes/menuRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs'); 
+app.set('views', path.join(__dirname, '../views')); 
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(express.static(path.join(__dirname, '../public'))); 
+
+app.use(menuRoutes); 
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущено на http://localhost:${PORT}`);
+});
