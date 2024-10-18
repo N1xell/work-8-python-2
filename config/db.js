50cf870790feba7110099cac270f5aa7069de426
N1xell <162792845+N1xell@users.noreply.github.com>
2024-10-18 22:03:41 +0300

    Update db.js

---
+++ config/db.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'your_username',     
+    password: 'your_password',  
+    database: 'your_database'   
+});
+
+module.exports = pool;
+
